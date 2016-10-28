cddb53b55ec33776dce998548ebded652de39541
ajilantang <ajilantang@gmail.com>
2016-10-28 21:18:18 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//---------------------------
+//Node modules
+//------------------------
+
+//express dependencies
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+
+//initiate express
+const app = express()
+const router = app.Router()
+
+//-----------------------------------------------
+// app configuration
+//------------------------------------------------
